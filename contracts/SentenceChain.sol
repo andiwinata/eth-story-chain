7197f14f538e285d0ae376aaa93e0baf53cf04d6
andiwinata <andi.winata@hotmail.com>
2017-12-16 06:28:45 +1100

    Adding SentenceChain contract and trying to see which one more optimized, hash or store operation

---
+++ contracts/SentenceChain.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+contract SentenceChain {
+  uint256 sentenceId = 0;
+  event SentenceCreated(address indexed author, uint256 indexed sentenceId, bytes32 sentence);
+  event SentenceCreatedHash(address indexed author, bytes32 indexed sentenceId, bytes32 sentence);
+
+  function createSentenceWithInternalId(bytes32 sentence) returns (uint256) {
+    sentenceId++;
+    SentenceCreated(msg.sender, sentenceId, sentence);
+  }
+
+  function createSentenceWithHash(bytes32 sentence) returns (bytes32) {
+    var hash = sha3(sentence);
+    SentenceCreatedHash(msg.sender, hash, sentence);
+  }
+}
